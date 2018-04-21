3fc14e177866088d1c4649b5711bca85046f8866
Marat Bunyatov <marat@Marats-MBP.home>
2018-04-21 19:37:02 -0400

    refactor

--- api/controllers/inventoryController.js
+++ api/controllers/inventoryController.js
@@ -1,15 +1,55 @@
 'use strict';
+import { getContracts } from "../../contracts";
 
 var db = require('../model/db');
 var CORS = 'http://localhost:8000';
+let TOKENS_LIST = [
+	'EosToken', 
+	'VenToken',
+  	'WethToken'
+];
+
+exports.tokens = function(req, res) {
+	res.setHeader('Access-Control-Allow-Origin', CORS);
+	let token = req.params.token;
+	let contracts = getContracts();
+	if (token == null) {
+		let result = {};
+		for(let c in contracts) {
+			if (TOKENS_LIST.includes(c)) {
+				result[c] = contracts[c];
+			}
+		}
+		res.json(result);
+	}
+	else {
+		res.json(contracts[token]);
+	}
+};
+
+exports.contracts = function(req, res) {
+	res.setHeader('Access-Control-Allow-Origin', CORS);
+	let id = req.params.id;
+	let contracts = getContracts();
+	if (id == null) {
+		res.json(contracts);
+	}
+	else {
+		res.json(contracts[id]);
+	}
+};
 
 exports.list_inventory = function(req, res) {
 	res.setHeader('Access-Control-Allow-Origin', CORS);
-	if (!(req.params.userId in db.inventory.getStore())) {
+	let userId = req.params.userId;
+	if (userId == null) {
+		res.json(db.inventory.getStore());
+	}
+	else if (!(userId in db.inventory.getStore())) {
 		res.json({});
 	}
-	else{
-		res.json(db.inventory.getStore()[req.params.userId]);
+	else {
+		res.json(db.inventory.getStore()[userId]);
 	}
 };
 
@@ -20,5 +60,14 @@ exports.add_inventory = function(req, res) {
 		req.body.token,
 		req.body.amount,
 		req.body.role,
+	));
+};
+
+exports.recall = function(req, res) {
+	res.setHeader('Access-Control-Allow-Origin', CORS);
+	res.json(db.inventory.addInventory(
+		req.params.userId,
+		req.body.token,
+		req.body.amount,
 	));
 };
--- api/routes/inventoryRoutes.js
+++ api/routes/inventoryRoutes.js
@@ -5,8 +5,20 @@ module.exports = function(app) {
   var inventory = require('../controllers/inventoryController');
 
   // inventory Routes
+  app.route('/tokens/')
+    .get(inventory.tokens);
+  app.route('/tokens/:token')
+    .get(inventory.tokens);
+  app.route('/contracts/')
+    .get(inventory.contracts);
+  app.route('/contracts/:id')
+    .get(inventory.contracts);
+  app.route('/inventory/')
+    .get(inventory.list_inventory);
   app.route('/inventory/:userId')
     .get(inventory.list_inventory);
   app.route('/inventory/:userId')
     .post(inventory.add_inventory);
+  app.route('/recall/:userId')
+    .post(inventory.recall);
 };
