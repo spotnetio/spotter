29adebee57da5d575c22788f6fadeed37e071c1f
Marat Bunyatov <marat@Marats-MBP.home>
2018-04-21 19:37:02 -0400

    refactor

--- api/model/db.js
+++ api/model/db.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const spotArtifact = require("../../../contracts/build/contracts/Spot.json");
 const TruffleContract = require("truffle-contract");
 const Web3 = require("web3");
@@ -21,18 +19,7 @@ const 	rate = 100;		// times 100
 const 	fee = 0;		// basis (10000)
 const 	margin = 10000;	// basis
 
-let store = {
-	// 'user': {
-	// 	'token1': {
-	// 		'lender': 1,
-	// 		'trader': 2,
-	// 	},
-	// 	'token2': {
-	// 		'lender': 3,
-	// 		'trader': 4,
-	// 	},
-	// },
-};
+let store = {};
 
 exports.inventory = {
 	getStore: function() {
@@ -53,88 +40,179 @@ exports.inventory = {
 			store[user][token][role] += parseInt(amount);
 		}
 		// console.log('store[user][token][role] '+store[user][token][role]);
-		this.findMatchAndLock(user, token, role, counterpartRole);
+		if(role == 'lender') {
+			this.findLenderMatchAndLock(user, token);
+		}
+		else {
+			this.findTraderMatchAndLock(user, token);
+		}
 	},
 
-	findMatchAndLock(user, token, role, counterpartRole) {
-		let amount = store[user][token][role];
-		// console.log('amount '+amount);
+	async findTraderMatchAndLock(user, token) {
+		let lenders = [];
+		let traders = await deployedSpot.getTraders();
+		let lockedTokens = await deployedSpot.getTokens();
+		let amount = store[user][token]['trader'];
 		let amountEther = Math.floor(amount*rate*(fee + margin)/1000000);
+		let traderIdx = traders.indexOf(user);
+		if(traderIdx >= 0) {
+			if (lockedTokens[traderIdx] == token) {
+				lenders = await deployedSpot.getLenders(traderIdx);
+			}
+			else {
+				traderIdx = -1;
+			}
+		}
 		for (let u in store) {
 			for (let t in store[u]) {
-				amount = store[user][token][role];
-				if (amount == 0){
-					return;
-					// console.log('store1 '+JSON.stringify(store));
-				}
-				// Amounts of X1 and ether to be filled on this pass
 				let x1Filled = Math.min(
 					amount,
-					store[u][t][counterpartRole] 
+					store[u][t]['lender'] 
 				);
-				// console.log('amountEther '+amountEther);
-				// console.log('store[u][t][counterpartRole] '+store[u][t][counterpartRole]);
 				let etherFilled = Math.min(
 					amountEther,
 					Math.floor(
-						store[u][t][counterpartRole]
+						store[u][t]['lender']
 							*rate*(fee + margin)/1000000
 					)
 				);
-				// console.log('x1Filled '+x1Filled);
-				// console.log('etherFilled '+etherFilled);
-				// console.log('deployedSpot.address '+deployedSpot.address);
 				if (t == token && x1Filled > 0 && etherFilled > 0) {
-					// call lock
-					if(role == 'lender') {
-						console.log('lenderLock('+
-							user+','+ 
-							u+',' +
-							token+','+
-							etherFilled+','+
-							rate+','+
-							fee+','+
-							margin+')'
-						);
-						deployedSpot.Lock(
-							user, 
-							u, 
-							token,
-							etherFilled,
-							rate,
-							fee,
-							margin,
-							{from: web3.eth.accounts[0], gas: 3000000}
-						);
-					}
-					else {
-						console.log('traderLock('+
-							u+',' +
-							user+','+ 
-							token+','+
-							etherFilled+','+
-							rate+','+
-							fee+','+
-							margin+')'
-						);
-						deployedSpot.Lock(
-							u, 
-							user, 
-							token,
-							amountEther,
-							rate,
-							fee,
-							margin,
-							{from: web3.eth.accounts[0], gas: 3000000}
-						);						
-					}
+					let lenderIdx = lenders.indexOf(u);
+					console.log('LockTrader('+
+						user+','+ 
+						traderIdx+','+ 
+						token+','+
+						x1Filled+','+
+						u+','+
+						lenderIdx+','+
+						rate+','+
+						fee+','+
+						margin+','+
+						'{from: '+web3.eth.accounts[0]+', gas: 3000000})'
+					);
+					deployedSpot.Lock(
+						user, 
+						traderIdx, 
+						token,
+						x1Filled,
+						u,
+						lenderIdx,
+						rate,
+						fee,
+						margin,
+						{from: web3.eth.accounts[0], gas: 3000000}
+					);
 					// subtract from inventory					
-					store[user][token][role] -= x1Filled;
-					store[u][t][counterpartRole] -= x1Filled;
+					store[user][token]['trader'] -= x1Filled;
+					store[u][t]['lender'] -= x1Filled;
 				}
 			}
 		}
-		// console.log('store2 '+JSON.stringify(store));
+	},
+
+	async findLenderMatchAndLock(user, token) {
+		let amount = store[user][token]['lender'];
+		let amountEther = Math.floor(amount*rate*(fee + margin)/1000000);
+		let traders = await deployedSpot.getTraders();
+		let lockedTokens = await deployedSpot.getTokens();
+		for (let u in store) {
+			for (let t in store[u]) {
+				let x1Filled = Math.min(
+					amount,
+					store[u][t]['trader'] 
+				);
+				let etherFilled = Math.min(
+					amountEther,
+					Math.floor(
+						store[u][t]['trader']
+							*rate*(fee + margin)/1000000
+					)
+				);
+				if (t == token && x1Filled > 0 && etherFilled > 0) {
+					let lenders = [];
+					let traderIdx = traders.indexOf(u);
+					if(traderIdx >= 0) {
+						if (lockedTokens[traderIdx] == token) {
+							lenders = await deployedSpot.getLenders(traderIdx);
+						}
+						else {
+							traderIdx = -1;
+						}
+					}
+					let lenderIdx = lenders.indexOf(user);
+					console.log('LockLender('+
+						u+','+ 
+						traderIdx+','+ 
+						token+','+
+						x1Filled+','+
+						user+','+
+						lenderIdx+','+
+						rate+','+
+						fee+','+
+						margin+','+
+						'{from: '+web3.eth.accounts[0]+', gas: 3000000})'
+					);
+					deployedSpot.Lock(
+						u, 
+						traderIdx, 
+						token,
+						x1Filled,
+						user,
+						lenderIdx,
+						rate,
+						fee,
+						margin,
+						{from: web3.eth.accounts[0], gas: 3000000}
+					);
+					// subtract from inventory					
+					store[user][token]['lender'] -= x1Filled;
+					store[u][t]['trader'] -= x1Filled;
+				}
+			}
+		}
+	},
+
+	recall(lender, token, amount) {
+		lenders = [];
+		amounts = [];
+loop1:
+		for (let u in store) {
+			for (let t in store[u]) {
+				swapAmount = Math.min(
+					store[u][t]['lender'], 
+					amount
+				);
+				if (t == token && swapAmount > 0) {
+					lenders.push(u);
+					amounts.push(swapAmount);
+					store[u][t]['lender'] -= swapAmount;
+					amount -= swapAmount
+					if (amount == 0) {
+						break loop1;
+					}
+				}
+			}
+		}
+		deployedSpot.Swap(
+			lender,
+			token,
+			lenders,
+			amounts,
+			rate, 
+			fee,
+			margin
+		);
+
+		if (amount > 0) {
+			let borrowers, amounts = deployedSpot.Recall(
+				lender,
+				token,
+				amount
+			);
+			for (let i=0; i<borrowers.length; i++) {
+				store[borrowers[i]][token]['lender'] += amounts[i];
+			}
+		}
 	}
 };
 
