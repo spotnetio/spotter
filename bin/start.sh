aa8c198b5b34316d0d46c5c696d1d122347b3978
Marat Bunyatov <marat@Marats-MBP.home>
2018-04-27 18:27:25 -0400

    single container

--- bin/start.sh
+++ bin/start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-if [ "${TARG}" = "spotter" ]; then
-	echo "npm run start"
-    cd /spotter && npm run start
-else
-	echo "httpserver"
-    cd /ui && httpserver
-fi
+cd /ui && sudo httpserver -p 80 &
+cd /spotter && npm run start &> /logs/spotter.log
