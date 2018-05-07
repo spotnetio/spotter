64f936477db37e9643f835a36ce168b5d0ee2bd0
Marat Bunyatov <marat@Marats-MBP.home>
2018-05-07 02:03:16 -0400

    available

--- contracts/local/index.js
+++ contracts/local/index.js
@@ -1,7 +1,7 @@
 import EOS from "../../../contracts/build/contracts/EOS.json";
 import TRX from "../../../contracts/build/contracts/TRX.json";
 import BNB from "../../../contracts/build/contracts/BNB.json";
-import ZeroX from "../../../contracts/build/contracts/ZeroX.json";
+import OMG from "../../../contracts/build/contracts/OMG.json";
 import Weth from "../../../contracts/build/contracts/Weth.json";
 import Spot from "../../../contracts/build/contracts/Spot.json";
 
@@ -9,7 +9,7 @@ export default {
   EOS,
   TRX,
   BNB,
-  ZeroX,
+  OMG,
   Weth,
   Spot,
 };
